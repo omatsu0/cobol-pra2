91e8f162e7f9318bc2f50d17bbcb30dee179017e
“sss-o2” <your@example.com>
2020-11-16 23:02:17 +0900

    lesson8

--- cobol-test/hello2.cbl
+++ cobol-test/hello2.cbl
@@ -2,13 +2,19 @@
 000010 PROGRAM-ID.    HELLO.
        DATA DIVISION.
          WORKING-STORAGE SECTION.
-            01 MY-COLORS.
-              03 MY-COLOR OCCURS 3 PIC X(10).
+          01 MY-DATA.
+              03 D1 PIC 99999 VALUE 0.
+              03 D2 PIC 99999 VALUE 0.
+              03 D3 PIC 99999 VALUE 0.
+              03 D4 PIC 99999 VALUE 0.
 000020 PROCEDURE      DIVISION.
 000030 MAIN           SECTION.
-       MOVE "red" TO MY-COLOR(1).
-       MOVE "blue" TO MY-COLOR(2).
-       MOVE "yellow" TO MY-COLOR(3).
-000040 DISPLAY        MY-COLORS.
-000040 DISPLAY        MY-COLOR(2).
-000050 STOP           RUN.
+            *> ADD 5 TO D1 D2 D3.
+            *> SUBTRACT 2 FROM D1 D2.
+            MOVE 10 TO D1.
+            MOVE 3 TO D2.
+            *> DIVIDE D1 BY D2 GIVING D3 REMAINDER D4.
+            *> MULTIPLY D1 BY D2 GIVING D3.
+            COMPUTE D3 = D1 * D2. *> + - / * **
+            DISPLAY MY-DATA.
+000040 STOP           RUN.
