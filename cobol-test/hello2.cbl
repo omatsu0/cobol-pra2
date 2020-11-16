a0b696c0adfd98c3aa6fc43efa13cbeb23bdf53c
“sss-o2” <your@example.com>
2020-11-16 23:13:08 +0900

    lesson10

--- cobol-test/hello2.cbl
+++ cobol-test/hello2.cbl
@@ -2,10 +2,13 @@
 000010 PROGRAM-ID.    HELLO.
        DATA DIVISION.
          WORKING-STORAGE SECTION.
-          01 MY-DATA PIC X(10).
+          01 MY-SCORE PIC 9(3).
 000020 PROCEDURE      DIVISION.
 000030 MAIN           SECTION.
-            DISPLAY "What's your name?".
-            ACCEPT MY-DATA.
-            DISPLAY "Hello!" MY-DATA.
+            MOVE 40 TO MY-SCORE.
+            IF MY-SCORE IS GREATER THAN 60 THEN
+            DISPLAY "OK!"
+            ELSE 
+              DISPLAY "NG!"
+            END-IF.
 000040 STOP           RUN.
