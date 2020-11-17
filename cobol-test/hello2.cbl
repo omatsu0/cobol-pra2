cc9d876da88b521b8b47ffbc9e9453cb83f15b5b
“sss-o2” <your@example.com>
2020-11-17 23:23:05 +0900

    lesson12

--- cobol-test/hello2.cbl
+++ cobol-test/hello2.cbl
@@ -2,17 +2,20 @@
 000010 PROGRAM-ID.    HELLO.
        DATA DIVISION.
          WORKING-STORAGE SECTION.
-          01 MY-SIGNAL PIC X(5).
+          01 MY-COUNTER PIC 9(3) VALUE 1.
 000020 PROCEDURE      DIVISION.
 000030   MAIN           SECTION.
-            MOVE "eeeaaa" TO MY-SIGNAL.
-            EVALUATE MY-SIGNAL
-              WHEN "red"
-              WHEN "green"
-                DISPLAY "GO!"
-              WHEN "yellow"
-                DISPLAY "Caution!"
-              WHEN OTHER
-                DISPLAY "Wrong Signal!"
-            END-EVALUATE.
+            *> PERFORM 10 TIMES
+            *>   DISPLAY "counter = " MY-COUNTER
+            *>   ADD 1 TO MY-COUNTER
+            *> END-PERFORM.
+
+            *> PERFORM UNTIL MY-COUNTER > 10
+            *>   DISPLAY "counter = " MY-COUNTER
+            *>   ADD 1 TO MY-COUNTER
+            *> END-PERFORM.
+
+            PERFORM VARYING MY-COUNTER FROM 1 BY 3 UNTIL MY-COUNTER > 10
+              DISPLAY "counter = " MY-COUNTER
+            END-PERFORM.
 000040      STOP  RUN.
