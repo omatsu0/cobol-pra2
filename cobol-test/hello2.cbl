5fbad5f8f57f7b5883b525b871a3ece6a8e43908
“sss-o2” <your@example.com>
2020-11-17 23:00:51 +0900

    lesson11

--- cobol-test/hello2.cbl
+++ cobol-test/hello2.cbl
@@ -2,13 +2,17 @@
 000010 PROGRAM-ID.    HELLO.
        DATA DIVISION.
          WORKING-STORAGE SECTION.
-          01 MY-SCORE PIC 9(3).
+          01 MY-SIGNAL PIC X(5).
 000020 PROCEDURE      DIVISION.
-000030 MAIN           SECTION.
-            MOVE 40 TO MY-SCORE.
-            IF MY-SCORE IS GREATER THAN 60 THEN
-            DISPLAY "OK!"
-            ELSE 
-              DISPLAY "NG!"
-            END-IF.
-000040 STOP           RUN.
+000030   MAIN           SECTION.
+            MOVE "eeeaaa" TO MY-SIGNAL.
+            EVALUATE MY-SIGNAL
+              WHEN "red"
+              WHEN "green"
+                DISPLAY "GO!"
+              WHEN "yellow"
+                DISPLAY "Caution!"
+              WHEN OTHER
+                DISPLAY "Wrong Signal!"
+            END-EVALUATE.
+000040      STOP  RUN.
